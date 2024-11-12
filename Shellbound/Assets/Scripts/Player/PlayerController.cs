e205109e6dd5fa0c7d9c51991615382791b94ffd
Adam Wijk <adamwijk2001@gmail.com>
2024-11-12 17:02:31 +0100

    Set up a basic player controller

--- Shellbound/Assets/Scripts/Player/PlayerController.cs
+++ Shellbound/Assets/Scripts/Player/PlayerController.cs
@@ -3,7 +3,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed;
+    public float maxSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
 
     public Transform orientation;
 
@@ -18,11 +30,16 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+
+        readyToJump = true;
     }
 
     private void Update()
     {
+        GroundCheck();
         GetInputs();
+        LimitToMaxSpeed();
+        HandleDrag();
     }
 
     private void FixedUpdate()
@@ -30,17 +47,68 @@ public class PlayerController : MonoBehaviour
         MovePlayer();
     }
 
+    private void GroundCheck()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+    }
+
     private void GetInputs()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButton("Jump") && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void HandleDrag()
+    {
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else rb.drag = 0;
     }
 
     private void MovePlayer()
     {
-        //calculate direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.velocity = moveDirection.normalized * moveSpeed;
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * maxSpeed * 10f, ForceMode.Force);
+        }
+
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * maxSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+    private void LimitToMaxSpeed()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (flatVelocity.magnitude > maxSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * maxSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
