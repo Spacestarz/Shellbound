9eef7024c0ddb87a49d14b968659ae4affaded95
Spacestarz <samromell96@gmail.com>
2024-11-14 11:37:33 +0100

    Fixed rotation of the rope. Also changed the name so it can be used universally
    (will need to be changed later it got a comment so should be easy to find)

--- Shellbound/Assets/Scripts/Player/Fire.cs
+++ Shellbound/Assets/Scripts/Player/Fire.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-    public GameObject Harpoon;
+    public GameObject Anchor;
     public GameObject rope;
+    public GameObject mainCam;
     public float fireRate = 2;
     public float speedReturn = 1;
 
-    public float maxDistanceHarpoon = 2f; //test this to see whats best
+    public float maxDistancefromAnchor = 2f; //test this to see whats best
+
 
     //bools
     bool fired = false;
@@ -26,14 +28,19 @@ public class Fire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //distance of the harpoon and rope
-        float dist = Vector3.Distance(Harpoon.transform.position, transform.position);
+        //distance of the Anchor and rope
+        float dist = Vector3.Distance(Anchor.transform.position, transform.position);
+        
 
         if (Input.GetButtonDown("Fire1"))
         {
             velocityZero = false;
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-            GetComponent<Rigidbody>().velocity = transform.up * fireRate * Time.deltaTime;
+
+            //it gets the same rotation as the main camera will probarly need to be changed when the real sprites gets implemented.  
+            transform.rotation = Quaternion.LookRotation(mainCam.transform.up, mainCam.transform.forward);
+           //It move the direction of the main cameras z axis
+            GetComponent<Rigidbody>().velocity =  mainCam.transform.forward * fireRate * Time.deltaTime;
 
             fired = true;
         }
@@ -41,7 +48,7 @@ public class Fire : MonoBehaviour
         //TODO AFTER X seconds it returns to the player //invoke
         //make it check the pos and move 10% towards it and then check where is the pos etc
         //Add methods to make the code cleaner
-        if (Input.GetButtonDown("Jump") || dist >= maxDistanceHarpoon)
+        if (Input.GetButtonDown("Jump") || dist >= maxDistancefromAnchor)
         {
             GetComponent<Rigidbody>().velocity = Vector3.zero;
             velocityZero = true;
@@ -50,12 +57,14 @@ public class Fire : MonoBehaviour
 
         if (velocityZero == true) 
         {
-            rope.transform.position = Vector3.MoveTowards(rope.transform.position, Harpoon.transform.position, speedReturn * Time.deltaTime);          
+            //TODO LATER
+            //to make this look nicer change it to a lerp.
+            rope.transform.position = Vector3.MoveTowards(rope.transform.position, Anchor.transform.position, speedReturn * Time.deltaTime);          
 
-            //if the distance of the rope and harpoon is below 10 it snaps to position and freezez
-          if ( dist <10)
+            //if the distance of the rope and Anchor is below 5 it snaps to position and freezez
+          if ( dist <5)
             {
-                rope.transform.position = Harpoon.transform.position;
+                rope.transform.position = Anchor.transform.position;
                 GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
                 velocityZero = false;
             }
