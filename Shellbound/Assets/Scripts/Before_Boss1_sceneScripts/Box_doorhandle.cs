66f6ad26383d684c383f8173ba3e9970bce61857
Spacestarz <samromell96@gmail.com>
2024-11-27 17:38:03 +0100

    working with pingpong clearly i am doing something wrong

--- Shellbound/Assets/Scripts/Before_Boss1_sceneScripts/Box_doorhandle.cs
+++ Shellbound/Assets/Scripts/Before_Boss1_sceneScripts/Box_doorhandle.cs
@@ -3,31 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using System;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 
 public class Box_doorhandle : MonoBehaviour
 {
     private Vector3 startpos;
     private Vector3 endpos;
-    private Vector3 direction;
-    private float distance = 2f;
+    private float pingPongResult;
+
+
+    private bool stopPingPong = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        direction = Vector3.forward;
         startpos = transform.position;
-        endpos = transform.position + direction * distance;
+        endpos = startpos + new Vector3 (0.5f, 0, 0);
+                  
+        Movement();       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
-       
+        //maybe do transform go there and then go there instead of dotween
+        //kolla på mathfpingpong
     }
 
     private void Movement()
     {
-        transform.DOMove(endpos, 3f);
+        if (stopPingPong == false)
+        {
+            // pingPongResult = Mathf.PingPong(Time.time,1);
+            transform.position = new Vector3(startpos.x + Mathf.PingPong(Time.time, 1), transform.position.y, transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {    
+        if (other.CompareTag("Harpoon"))
+        {
+            stopPingPong = true;
+            Debug.Log("Auch said the glove");
+
+            transform.DOMove(startpos, 1f);
+
+            Debug.Log("IM HOME NOW YOU BAD");
+        }       
     }
 }
