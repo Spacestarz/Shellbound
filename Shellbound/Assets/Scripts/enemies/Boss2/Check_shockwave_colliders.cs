da4f7ed63aacf0411a57f5dee2afc78cda3c23ba
Spacestarz <samromell96@gmail.com>
2024-11-29 14:28:18 +0100

    WIP WIP
    trying with the  shockwave for boss 2

---
+++ Shellbound/Assets/Scripts/enemies/Boss2/Check_shockwave_colliders.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check_shockwave_colliders : MonoBehaviour
+{
+
+    //if player are in one spere take damage
+    //if the player are in both DONT take damage
+
+    //use on triggerstay
+
+    public GameObject player;
+    private Collider SpereColliders;
+    private Collider MaskCollider;
+
+    public bool outerSpere = false;
+    public bool innerSpere = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpereColliders = GetComponent<Collider>();
+        MaskCollider = GetComponentInChildren<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag ("Player"))
+        {
+            Debug.Log("Hello player");
+        }
+
+        if (other.CompareTag("Inner spere"))
+        {
+            Debug.Log("Inner spere");
+        }
+
+        if (other.CompareTag("Outer Spere"))
+        {
+            Debug.Log("Outer Spere");
+        }
+     
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+        }
+    }
+}
