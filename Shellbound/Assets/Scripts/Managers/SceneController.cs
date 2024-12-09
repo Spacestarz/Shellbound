e4a20ea6a28d2031766b2473c138030457e486bc
Alkr04 <h23alkr@axevalla.fhsk.se>
2024-12-09 16:31:57 +0100

    created a funktional start menue

--- Shellbound/Assets/Scripts/Managers/SceneController.cs
+++ Shellbound/Assets/Scripts/Managers/SceneController.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("GameController");
+        if(obj.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            GoToMenue();
+        }
+        else if (Input.GetKey(KeyCode.Backspace))
+        {
+            restart();
+        }
+        //debug
+        else if (Input.GetKey(KeyCode.Keypad6))
+        {
+            NextLevel();
+        }
+    }
+    public void quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+        Application.OpenURL(webplayerQuitURL);
+#else
+        Application.Quit();
+#endif
+    }
+    public void restart()
+    {
+        var scean = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(scean);
+    }
+    public void GoToMenue()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void NextLevel()
+    {
+        var scean = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(scean + 1);
     }
 }
