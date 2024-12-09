47beb7196b9f3f888aceab3cd5c4a6090a8585cf
Alkr04 <h23alkr@axevalla.fhsk.se>
2024-12-09 11:45:40 +0100

    made the boss wait before going to next phase

--- Shellbound/Assets/Scripts/enemies/boss1/Boss1_AI.cs
+++ Shellbound/Assets/Scripts/enemies/boss1/Boss1_AI.cs
@@ -8,23 +8,40 @@ public class Boss1_AI : MonoBehaviour
     public List<base_enemi_attack> phases;
     public base_enemi_attack phase;
     Enemi_health health;
+    Base_enemy enemy;
+    bool PhaseSwitch = false;
 
     private void Awake()
     {
         phase = phases[0];
         health = GetComponent<Enemi_health>();
+        enemy = GetComponent<Base_enemy>();
     }
     private void Update()
     {
         phase.phase();
-        if (health.currentHP < health.MaxHP * 0.3)
+        if (health.currentHP < health.MaxHP * 0.3 && phase == phases[1] && !PhaseSwitch)
         {
-            phase = phases[2];
+            //phase = phases[2];
+            StartCoroutine(wait(3, 2));
         }
-        else if (health.currentHP < health.MaxHP * 0.7)
+        else if (health.currentHP < health.MaxHP * 0.7 && phase == phases[0] && !PhaseSwitch)
         {
-            phase = phases[1];
+            //phase = phases[1];
+            StartCoroutine(wait(3, 1));
         }
+    }
+
+    IEnumerator wait(float time, int NewPhase)
+    {
+        enemy.stop();
+        enemy.atta = false;
+        PhaseSwitch = true;
+        yield return new WaitForSeconds(time);
+        phase = phases[NewPhase];
+        PhaseSwitch = false;
+        enemy.atta = true;
+        enemy.start();
     }
 
 
