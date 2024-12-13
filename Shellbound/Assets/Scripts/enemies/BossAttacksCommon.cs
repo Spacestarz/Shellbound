9c16a44d324f46ee58bb244a1ab591dffb0cdbac
Alkr04 <h23alkr@axevalla.fhsk.se>
2024-12-13 11:41:44 +0100

    boss2 attacks working and phase 1 and 2 working

--- Shellbound/Assets/Scripts/enemies/BossAttacksCommon.cs
+++ Shellbound/Assets/Scripts/enemies/BossAttacksCommon.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class BossAttacksCommon : MonoBehaviour
 {
+    public bool velo = false;
     // Start is called before the first frame update
-    void Start()
+    public void BeVisible(GameObject obj)
     {
-        
+
+
+        var renderer = obj.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.enabled = true;
+        }
+
+        var spriteRenderer = obj.GetComponentInChildren<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = true;
+        }
+
+        var line = obj.GetComponent<MovingLine>();
+        if (line != null)
+        {
+            line.SetVisible(true);
+        }
+        obj.GetComponent<Collider>().enabled = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BeInvisible(GameObject obj)
     {
-        
+
+
+        var renderer = obj.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.enabled = false;
+        }
+
+        var spriteRenderer = obj.GetComponentInChildren<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = false;
+        }
+
+        var line = obj.GetComponent<MovingLine>();
+        if (line != null)
+        {
+            line.SetVisible(false);
+        }
+        obj.GetComponent<Collider>().enabled = false;
     }
 }
