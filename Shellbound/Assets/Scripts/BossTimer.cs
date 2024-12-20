81b71cd463f1642b09d247392df45b04341c9c75
Spacestarz <samromell96@gmail.com>
2024-12-20 10:14:30 +0100

    WIP with highsciore

--- Shellbound/Assets/Scripts/BossTimer.cs
+++ Shellbound/Assets/Scripts/BossTimer.cs
@@ -8,19 +8,32 @@ public class BossTimer : MonoBehaviour
     public bool TimerRunning = false;
     private static float timer = 0;
     private float bestTime;
-    public TextMeshProUGUI timerText;
+   // public TextMeshProUGUI timerText;
     
 
     public HealthSystem healthSystem;
 
     /*
-     * Make so the player can have a "highscore" how fast they killed the boss
+     * Make so we can have top 5 high scores
      * 
      * */
     // Start is called before the first frame update
     void Start()
     {
         TimerRunning = true;
+        bestTime = PlayerPrefs.GetFloat("Best Timer", float.MaxValue);
+        //timerText.text = "You best time is" + bestTime.ToString("F0");
+
+        if (bestTime == float.MaxValue)
+        {
+           // timerText.text = "No high score yet!";
+           Debug.Log("no high score yet");
+        }
+        else
+        {
+            Debug.Log("you best time is" + bestTime.ToString("F0"));
+            //timerText.text = ("you best time is" + bestTime.ToString("F0")); 
+        }
     }
 
     // Update is called once per frame
@@ -40,7 +53,17 @@ public class BossTimer : MonoBehaviour
     public void StopTimer()
     {
         TimerRunning = false;
-        timerText.text = "Your time to kill this boss was" + " " + (int)timer + "" + "seconds";
-        Debug.Log("your time was" + " " +  (int)timer);
+       // timerText.text = "Your time to kill this boss was" + " " + timer.ToString("F0");
+        Debug.Log("your time was" + " " + timer.ToString("F0"));
+
+        if (timer < bestTime)
+        {
+            bestTime = timer;
+            PlayerPrefs.SetFloat("Best Timer", bestTime);
+            PlayerPrefs.Save();
+            Debug.Log("New Best Timer: " + bestTime.ToString("F0"));
+
+        }       
+        //save string in const variable
     }
 }
