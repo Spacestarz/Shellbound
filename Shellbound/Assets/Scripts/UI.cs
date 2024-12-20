b5502fc01e9dc902e7c3e71ff9b3aba3619a225d
Adam Wijk <adamwijk2001@gmail.com>
2024-12-20 10:21:51 +0100

    Fixed further issues

--- Shellbound/Assets/Scripts/UI.cs
+++ Shellbound/Assets/Scripts/UI.cs
@@ -82,7 +82,7 @@ public class UI : MonoBehaviour
     public void DefeatedBOSS()
     {
         player.GetComponent<PlayerController>().enabled = false;
-        gameoverBLACK.SetActive(true);
+        //gameoverBLACK.SetActive(true);
 
         defeatedbossBOOL = true;
     }
