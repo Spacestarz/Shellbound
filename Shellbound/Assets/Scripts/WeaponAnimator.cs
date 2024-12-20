af2b5cb359411e6247549f0b2e19795008102562
Adam Wijk <adamwijk2001@gmail.com>
2024-12-20 12:03:00 +0100

    Implemented new harpoon

--- Shellbound/Assets/Scripts/WeaponAnimator.cs
+++ Shellbound/Assets/Scripts/WeaponAnimator.cs
@@ -1,57 +1,79 @@
 using Spine.Unity;
 using UnityEngine;
 using DG.Tweening;
+using Spine;
 
 public class WeaponAnimator : BaseAnimator
 {
     SkeletonGraphic skeletonGraphic;
     [SerializeField] private Rigidbody playerRb;
-    private Fire playerFire;
+
+    private Fire fire;
+    PlayerController controller;
+    private FireDart dartFire;
 
     RectTransform rect;
     Vector3 originalPos;
     Vector3 middlePos;
 
     bool playerIsWalking;
+
     bool harpoonIsFired;
+    bool dartIsFired;
+
+    bool harpoonWasActive;
+    public bool harpoonIsActive;
 
     Vector3 flatVelo;
 
     // Start is called before the first frame update
     void Awake()
     {
-        playerFire = playerRb.GetComponent<Fire>();
-        
+        fire = playerRb.GetComponent<Fire>();
+        controller = playerRb.GetComponent<PlayerController>();
+        dartFire = playerRb.GetComponent<FireDart>();
+
         skeletonGraphic = GetComponent<SkeletonGraphic>();
         
         rect = GetComponent<RectTransform>();
         originalPos = rect.anchoredPosition;
-        middlePos = new Vector3(-400, 32, 0);
+        middlePos = new Vector3(-0, -208, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
         flatVelo = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
-        CheckHarpoonFired();
+
+        CheckIfTimeToSwitch();
+
         CheckPlayerWalking();
+
+        if(harpoonIsActive)
+        {
+            Debug.Log("Harpy");
+            CheckHarpoonFired();
+        }
+        else
+        {
+            Debug.Log("dary");
+            CheckDartFired();
+        }
     }
 
 
     void CheckHarpoonFired()
     {
-        if (playerFire.fired && !harpoonIsFired)
+        if (fire.fired && !harpoonIsFired)
         {
             harpoonIsFired = true;
             
-            skeletonGraphic.AnimationState.SetAnimation(0, "shothold", false);
+            skeletonGraphic.AnimationState.SetAnimation(0, "Hook shot", false);
             skeletonGraphic.AnimationState.GetCurrent(0).TimeScale = 3.0f;
-
+            
+            Invoke(nameof(HasShot), 0.189f);
             rect.DOAnchorPos(middlePos, 0.189f);
-            //skeletonGraphic.AnimationState.AddAnimation(0, "shotstill", false, 0.063f);
-            //Invoke(nameof(TriggerShotStill), 0.063f);
         }
-        else if (!playerFire.fired && harpoonIsFired)
+        else if (!fire.fired && harpoonIsFired)
         {
             harpoonIsFired = false;
             ReturnToStill(ref harpoonIsFired);
@@ -59,23 +81,108 @@ public class WeaponAnimator : BaseAnimator
         }
     }
 
+
+    void CheckDartFired()
+    {
+        if (dartFire.shot && !dartIsFired)
+        {
+            dartIsFired = true;
+
+            //skeletonGraphic.AnimationState.SetAnimation(0, "no Hook or Dart bolt", false);
+            //skeletonGraphic.AnimationState.SetAnimation(0, "Dart bolt shot", false);
+            //skeletonGraphic.AnimationState.GetCurrent(0).TimeScale = 3.0f;
+
+            //Invoke(nameof(HasShot), 0.189f);
+            //rect.DOAnchorPos(middlePos, 0.189f);
+        }
+        else if (!dartFire.shot && dartIsFired)
+        {
+            dartIsFired = false;
+            ReturnToStill(ref harpoonIsFired);
+            //rect.DOAnchorPos(originalPos, 0.189f);
+        }
+    }
+
     void CheckPlayerWalking()
     {
-        if (flatVelo.magnitude > 0.1 && !playerIsWalking && !harpoonIsFired)
+        if (harpoonIsActive)
         {
-            playerIsWalking = true;
-            skeletonGraphic.AnimationState.SetAnimation(0, "bob", true);
+            if (flatVelo.magnitude > 0.1 && !playerIsWalking && !harpoonIsFired)
+            {
+                playerIsWalking = true;
+                skeletonGraphic.AnimationState.SetAnimation(0, "Hook walking bob", true);
+            }
+            else if (flatVelo.magnitude < 0.1 && playerIsWalking && !harpoonIsFired)
+            {
+                ReturnToStill(ref playerIsWalking);
+                playerIsWalking = false;
+            }
         }
-        else if (flatVelo.magnitude < 0.1 && playerIsWalking && !harpoonIsFired)
+        else
         {
-            playerIsWalking = false;
-            ReturnToStill(ref playerIsWalking);
+            if (flatVelo.magnitude > 0.1 && !playerIsWalking && !harpoonIsFired)
+            {
+                playerIsWalking = true;
+                skeletonGraphic.AnimationState.SetAnimation(0, "Dart bolt walking bob", true);
+            }
+            else if (flatVelo.magnitude < 0.1 && playerIsWalking && !harpoonIsFired)
+            {
+                playerIsWalking = false;
+                ReturnToStill(ref playerIsWalking);
+            }
         }
+    }
+
+    void HasShot()
+    {
+        skeletonGraphic.AnimationState.SetAnimation(0, "no Hook or Dart bolt", true);
     }
 
     void ReturnToStill(ref bool animBool)
     {
         animBool = false;
-        skeletonGraphic.AnimationState.SetAnimation(0, "still", false);
+        if(harpoonIsActive)
+        {
+            skeletonGraphic.AnimationState.SetAnimation(0, "Hook still", false);
+        }
+        else if(!PlayerSlice.instance.caughtObject)
+        {
+            skeletonGraphic.AnimationState.SetAnimation(0, "Dart bolt still", false);
+            skeletonGraphic.AnimationState.GetCurrent(0).TimeScale = 3.0f;
+        }
+
+        if(rect.anchoredPosition != (Vector2)originalPos)
+        {
+            rect.DOAnchorPos(originalPos, 0.189f);
+        }
+    }
+
+    public void CheckIfTimeToSwitch()
+    {
+        harpoonIsActive = controller.harpoontime;
+        if(harpoonIsActive != harpoonWasActive)
+        {
+            if(controller.harpoontime)
+            {
+                SwitchToHarpoon();
+            }
+            else if(!controller.harpoontime && !PlayerSlice.instance.caughtObject)
+            {
+                SwitchToDart();
+            }
+        }
+        harpoonWasActive = harpoonIsActive;
+    }
+
+    public void SwitchToHarpoon()
+    {
+        Debug.Log("Switch to Harp");
+        skeletonGraphic.AnimationState.SetAnimation(0, "Switch Dart bolt to Hook", false);
+    }
+
+    public void SwitchToDart()
+    {
+        Debug.Log("Switch to Dart");
+        skeletonGraphic.AnimationState.SetAnimation(0, "Switch Hook to Dart bolt", false);
     }
 }
